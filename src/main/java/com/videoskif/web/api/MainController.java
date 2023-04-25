d9ee34924d7ee65305a2026631a36047a7af6adf
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2023-04-25 18:31:31 +0200

    init

--- src/main/java/com/videoskif/web/api/MainController.java
+++ src/main/java/com/videoskif/web/api/MainController.java
@@ -2,17 +2,11 @@ package com.videoskif.web.api;
 
 import com.videoskif.service.GenerationOrchestrator;
 import com.videoskif.service.S3Service;
-import com.videoskif.web.api.dto.SaveImageRequest;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -26,11 +20,6 @@ public class MainController {
   @GetMapping("/topic/{topicName}")
   public List<String> generateVideosByTopic(@PathVariable String topicName){
     return orchestrator.generateVideos(topicName);
-  }
-
-  @PostMapping("/image")
-  public String saveImage(@RequestBody SaveImageRequest request){
-    return s3Service.saveImageToStore(request.getSrc(), "Image-1");
   }
 
 }
